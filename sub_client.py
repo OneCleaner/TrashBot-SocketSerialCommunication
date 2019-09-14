78261f1afd65d6c13eb52e5809d6e92fffe4516e
Federico Gerardi <federico.gerardi03@gmail.com>
2019-09-14 17:22:54 +0200

    Gui

--- sub_client.py
+++ sub_client.py
@@ -1,28 +1,97 @@
 # Questo programma va eseguito sul PC
 
+from tkinter import Tk, Label, Entry, Button, PhotoImage, Scale, HORIZONTAL
 import socket
 import sys
 
+comando = ""
+
+def avanti():
+    global comando
+    comando = "dritto"
+
+
+def retro():
+    global comando
+    comando = "retro"
+
+
+def destra():
+    global comando
+    comando = "destra"
+
+
+def sinistra():
+    global comando
+    comando = "sinistra"
+
+
+def accendi():
+    global comando
+    comando = "accendi"
+
+
+def spegni():
+    global comando
+    comando = "spegni"
+
+
+def esc():
+    global comando
+    comando = "ESC"
+
+vel2 = 0
+
+def vel():
+    global comando, vel2
+    comando = str(vel2)
+
 
 def invia_comandi(skt):
-    print('- Inserire "destra" per svoltare a destra')
-    print('- Inserire "sinistra" per svoltare a sinistra')
-    print('- Inserire "spegni" per spegnere')
-    print('- Inserire "accendi" per accendere')
-    print('- Inserire un numero tra 190 e 255 per cambiare la velocità')
-    print('- Inserire "ESC" per uscire\n\n')
-    while True:
-        comando = input("-> ")
-        if comando == "ESC":
-            print("Sto chiudendo la connessione con il Raspberry")
-            skt.send(comando.encode())
-            skt.shutdown(socket.SHUT_RDWR) #solo client
-            skt.close()
-            sys.exit()
-        else:
-            skt.send(comando.encode())
-            data = skt.recv(4096)
-            print(str(data, "utf-8"))
+    global comando, vel2
+
+    root = Tk()
+    root.title("Controllo Raspy")
+    root.geometry("300x200")
+    root.resizable(False, False)
+    load_arrow_up = PhotoImage(file="arrow-up-solid.png")
+    arrow_up = load_arrow_up.subsample(50, 50)
+    load_arrow_down = PhotoImage(file="arrow-down-solid.png")
+    arrow_down = load_arrow_down.subsample(50, 50)
+    load_arrow_left = PhotoImage(file="arrow-left-solid.png")
+    arrow_left = load_arrow_left.subsample(50, 50)
+    load_arrow_right = PhotoImage(file="arrow-right-solid.png")
+    arrow_right = load_arrow_right.subsample(50, 50)
+    Button(root, image=arrow_up, command=avanti).place(x=60, y=20)
+    Button(root, image=arrow_down, command=retro).place(x=60, y=100)
+    Button(root, image=arrow_left, command=sinistra).place(x=20, y=60)
+    Button(root, image=arrow_right, command=destra).place(x=100, y=60)
+    Button(root, text="Accendi", command=accendi).place(x=220, y=40)
+    Button(root, text=" Spegni ", command=spegni).place(x=220, y=70)
+    Button(root, text="    Esc    ", command=esc).place(x=220, y=100)
+    vel1 = Scale(root, from_=90, to=255, orient=HORIZONTAL).place(x=50, y=150)
+    vel2 = vel1.get()
+    Button(root, text="Invia Vel", command=vel).place(x=170, y=165)
+
+    if comando == "ESC":
+        print("Sto chiudendo la connessione con il Raspberry")
+        skt.send(comando.encode())
+        skt.shutdown(socket.SHUT_RDWR)  # solo client
+        skt.close()
+        sys.exit()
+    elif comando != "":
+        skt.send(comando.encode())
+        data = skt.recv(4096)
+        print(str(data, "utf-8"))
+    comando = ""
+
+    root.mainloop()
+
+def access_connection():
+    ip = ip_entry.get()
+    door = door_entry.get()
+    access.destroy()
+    connessione_server((ip, int(door)))
 
 
 
@@ -30,16 +99,38 @@ def connessione_server(indirizzo_server):
     try:
         skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         skt.connect(indirizzo_server)
-        print("Connessione al Raspberry Riuscita!")
     except socket.error as errore:
         print("Connessione Fallita: \n" + str(errore))
         skt.shutdown(socket.SHUT_RDWR)
         skt.close()
         sys.exit()
     else:
+        print("Connessione al Raspberry Riuscita!")
         invia_comandi(skt)
 
+
+
+
+
 if __name__ == "__main__":
-    IP_RASPBERRY = input("Inserisci l'indirizzo del Raspberry \n-> ")
-    PORTA_RASPBERRY = input("Inserisci la porta che hai inserito nel Raspberry \n-> ")
-    connessione_server((IP_RASPBERRY, int(PORTA_RASPBERRY)))
+
+    access = Tk()
+    access.title("Collegamento Raspy")
+    access.geometry("300x300")
+    access.resizable(False, False)
+    Label(access, text="Indirizzo IP Raspy: ").place(x=30, y=20)
+    Label(access, text="Porta Raspy: ").place(x=30, y=50)
+    ip_entry = Entry(access)
+    ip_entry.place(x=140, y=20)
+    door_entry = Entry(access)
+    door_entry.place(x=140, y=50)
+    Button(access, text="Invia", command=access_connection).pack(pady="100")
+    access.mainloop()
+
+
+
+
+
+
+
+
--- sub_server.py
+++ sub_server.py
@@ -1,10 +1,11 @@
 # Questo programma va eseguito sul Raspberry
 
 import socket
-import sys
 from serial import Serial
+import platform
 
 def server(indirizzo, serial, backlog=1):
+
     try:
         skt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    #apriamo la comunicazione tramite socket
         skt.bind(indirizzo)
@@ -17,23 +18,31 @@ def server(indirizzo, serial, backlog=1):
     else:   #se non esplode stabiliamo la connessione
         conn, indirizzo_client = skt.accept()  # conn = socket_client
         print("Connessione Server - Client Stabilita: " + str(indirizzo_client))
+
         while True:
-            richiesta = conn.recv(4096)
-            if richiesta.decode() == "ESC":
-                print("Connessione con Client terminata, ritorno in ascolto")
+            try:
+                richiesta = conn.recv(4096)
+            except ConnectionResetError:        #se il client chiude forzatamente la connessione ci rimettiamo in ascolto
+                print("\n\nConnessione persa, ritorno in ascolto")
                 skt.close()
                 server(indirizzo, serial, backlog=1)
 
-            serial.write(str.encode(richiesta.decode()))   #manda sulla seriale di arduino i comandi ricevuti dal client
+            if richiesta.decode() == "ESC":    #se il client afferma di voler terminare la connessione ci rimettiamo in ascolto
+                print("\n\nConnessione con Client terminata, ritorno in ascolto")
+                skt.close()
+                server(indirizzo, serial, backlog=1)
+
+            serial.write(str.encode(richiesta.decode()))   #manda sulla seriale di arduino i comandi ricevuti dal client, si potrebbe fare senza codificare e decodificare ma noi siamo matti
             toRead = serial.readline()
             if toRead:
                 conn.sendall(("Comando ricevuto con successo! \n" + toRead.decode()).encode())
 
 if __name__ == "__main__":      #eseguiamo soltanto se questa è la classe main
-    #port = "COM3"
-    port = "/dev/tty"  # la porta
-    p = input("Inserire l'ultima parte della porta di arduino (escluso tty) \n-> ")    #prendiamo la seriale di arduino
-    port += p
+
+    if platform.system() == "Windows":
+        port = "COM" + input("Inserire il numero della porta di arduino \n-> ") #windows
+    else:
+        port = "/dev/tty" + input("Inserire l'ultima parte della porta di arduino (escluso tty) \n-> ")    #prendiamo la seriale di arduino
     serial = Serial(port, 9600)  #inizializziamo la comunicazione con arduino
     serial.flushInput()
     PORTA_SERVER = input("Inserisci una porta (Qualsiasi numero tra 1024 e 65000) \n-> ")   #prendiamo la porta su cui aprire la comunicazione con il PC
