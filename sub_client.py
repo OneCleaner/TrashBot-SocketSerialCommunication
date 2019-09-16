6c693fdd53d5c7d8c0c7e46a6e20c3447d40e8bf
Federico Gerardi <federico.gerardi03@gmail.com>
2019-09-16 19:52:36 +0200

    Comandi da tastiera

--- sub_client.py
+++ sub_client.py
@@ -4,13 +4,23 @@ from tkinter import Tk, Label, Entry, Button, PhotoImage, Scale, HORIZONTAL
 import socket
 import sys
 
+
 def avanti():
     skt.send(b"dritto")
     data = skt.recv(4096)
     print(str(data, "utf-8"))
 
+def avanti1(self):
+    skt.send(b"dritto")
+    data = skt.recv(4096)
+    print(str(data, "utf-8"))
 
 def retro():
+    skt.send(b"retro")
+    data = skt.recv(4096)
+    print(str(data, "utf-8"))
+
+def retro1(self):
     skt.send(b"retro")
     data = skt.recv(4096)
     print(str(data, "utf-8"))
@@ -21,8 +31,18 @@ def destra():
     data = skt.recv(4096)
     print(str(data, "utf-8"))
 
+def destra1(self):
+    skt.send(b"destra")
+    data = skt.recv(4096)
+    print(str(data, "utf-8"))
+
 
 def sinistra():
+    skt.send(b"sinistra")
+    data = skt.recv(4096)
+    print(str(data, "utf-8"))
+
+def sinistra1(self):
     skt.send(b"sinistra")
     data = skt.recv(4096)
     print(str(data, "utf-8"))
@@ -33,8 +53,18 @@ def accendi():
     data = skt.recv(4096)
     print(str(data, "utf-8"))
 
+def accendi1(self):
+    skt.send(b"accendi")
+    data = skt.recv(4096)
+    print(str(data, "utf-8"))
+
 
 def spegni():
+    skt.send(b"spegni")
+    data = skt.recv(4096)
+    print(str(data, "utf-8"))
+
+def spegni1(self):
     skt.send(b"spegni")
     data = skt.recv(4096)
     print(str(data, "utf-8"))
@@ -45,9 +75,19 @@ def esc():
     data = skt.recv(4096)
     print(str(data, "utf-8"))
 
+def esc1(self):
+    skt.send(b"ESC")
+    data = skt.recv(4096)
+    print(str(data, "utf-8"))
+
 # vel2 = 0
 
 def vel():
+    skt.send(str(vel1.get()).encode())
+    data = skt.recv(4096)
+    print(str(data, "utf-8"))
+
+def vel4(self):
     skt.send(str(vel1.get()).encode())
     data = skt.recv(4096)
     print(str(data, "utf-8"))
@@ -68,7 +108,6 @@ if __name__ == "__main__":
         sys.exit()
     else:
         print("Connessione al Raspberry Riuscita!")
-        global comando, vel2
 
         root = Tk()
         root.title("Controllo Raspy")
@@ -83,11 +122,17 @@ if __name__ == "__main__":
         load_arrow_right = PhotoImage(file="arrow-right-solid.png")
         arrow_right = load_arrow_right.subsample(50, 50)
         Button(root, image=arrow_up, command=avanti).place(x=60, y=20)
+        root.bind("w", avanti1)
         Button(root, image=arrow_down, command=retro).place(x=60, y=100)
+        root.bind("s", retro1)
         Button(root, image=arrow_left, command=sinistra).place(x=20, y=60)
+        root.bind("a", sinistra1)
         Button(root, image=arrow_right, command=destra).place(x=100, y=60)
+        root.bind("d", destra1)
         Button(root, text="Accendi", command=accendi).place(x=220, y=40)
+        root.bind("q", accendi1)
         Button(root, text=" Spegni ", command=spegni).place(x=220, y=70)
+        root.bind("e", spegni1)
         Button(root, text="    Esc    ", command=esc).place(x=220, y=100)
         vel1 = Entry(root, width=20)
         vel1.place(x=50, y=150)
